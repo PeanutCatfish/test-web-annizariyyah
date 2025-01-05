3ff6fd487d01a34e3a8fc08ecf870381d95fe7a1
PeanutCatfish <116542858+PeanutCatfish@users.noreply.github.com>
2025-01-05 10:20:41 +0700

    Add files via upload

---
+++ page-lain/send-email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "ikanlele958@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Pesan berhasil terkirim!";
+    } else {
+        echo "Pesan gagal dikirim.";
+    }
+}
+?>
